c023ece84fa343f4c82e8e1ed429911dd535ab09
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-17 01:37:34 +0200

    jwt settings done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -22,6 +24,20 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
@@ -30,7 +46,16 @@ async function run() {
 
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
-
+        //   jwt:
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn: '1h'
+            })
+            res.send({token});
+        })
+        // services routes
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -52,8 +77,13 @@ async function run() {
 
 
         // bookings 
-        app.get('/bookings', async (req, res) => {
-            console.log(req.query.email);
+        app.get('/bookings',verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('came back after verify', decoded)
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error: 1, message: 'forbidden access'})
+            }
+
             let query = {};
             // sudhu email er maddhome data find korar system:
             if (req.query?.email) {
@@ -70,26 +100,26 @@ async function run() {
             res.send(result);
         });
 
-        // app.patch('/bookings/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) };
-        //     const updatedBooking = req.body;
-        //     console.log(updatedBooking);
-        //     const updateDoc = {
-        //         $set: {
-        //             status: updatedBooking.status
-        //         },
-        //     };
-        //     const result = await bookingCollection.updateOne(filter, updateDoc);
-        //     res.send(result);
-        // })
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedBooking = req.body;
+            console.log(updatedBooking);
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
-        // app.delete('/bookings/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await bookingCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
